0ce2b604c4e6ef4d7801deba29bfb6390030a008
brandon10118 <202310118@fit.edu.ph>
2025-06-24 19:16:36 +0800

    refactor(checkers): update MongoDB and PostgreSQL connection configurations to use environment variables

--- utils/envSetter.util.php
+++ utils/envSetter.util.php
@@ -4,6 +4,15 @@ $dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
 $dotenv->load();
 
 // Distribute the data using array key
-$typeConfig = [
-    'key' => $_ENV['ENV_NAME'],
+$pgConfig = [
+    'host' => $_ENV['PG_HOST'],
+    'port' => $_ENV['PG_PORT'],
+    'db' => $_ENV['PG_DB'],
+    'user' => $_ENV['PG_USER'],
+    'pass' => $_ENV['PG_PASS']
+];
+
+$mongoConfig = [
+    'uri' => $_ENV['MONGO_URI'],
+    'db' => $_ENV['MONGO_DB']
 ];
